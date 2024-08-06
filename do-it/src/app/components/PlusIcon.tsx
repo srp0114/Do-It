494dbc918fce62ce4a8fda71dd55bd0c05a76e82
srp0114 <ksy010114@gmail.com>
2024-08-07 03:30:55 +0900

    design: 버튼 icon 크기 조정

--- do-it/src/app/components/PlusIcon.tsx
+++ do-it/src/app/components/PlusIcon.tsx
@@ -7,8 +7,6 @@ interface PlusIconProps {
 
 const PlusIcon: React.FC<PlusIconProps> = ({ className, ...props }) => (
     <svg
-        width="16"
-        height="16"
         viewBox="0 0 16 16"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
